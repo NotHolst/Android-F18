e9280eb355f0ffe3ba26fb3143bb5568b7c99a5a
Holst <moviemastersdk@gmail.com>
2018-05-19 22:06:45 +0200

    Server socket.io scaffolding

---
+++ AndroidChatServer/database/index.js
@@ -0,0 +1,64 @@
+const db = require('sqlite-sync');
+
+
+var facade = {
+
+    createUser(username, password, nickname) {
+        let res = db.run(
+            "INSERT INTO Users (Username, Password, Nickname, Status) VALUES (?,?,?,'Offline')",
+            [username, password, nickname]
+        )
+
+        return res.error ? -1 : res;
+
+    },
+
+    /**
+     * Authenticates and returns the user on success
+     * @param {string} username 
+     * @param {string} password 
+     */
+    auth(username, password) {
+        let res = db.run("SELECT * FROM Users WHERE Username = ? AND Password = ?");
+        return res.length == 1 ? res[0] : undefined;
+    },
+
+    createRoom(userID, otherUserID) {
+        let roomID = db.run("INSERT INTO Rooms (ID) VALUES (null)");
+        db.run("INSERT INTO RoomMemberships (UserID, RoomID) VALUES (?,?)", [userID, roomID]);
+        db.run("INSERT INTO RoomMemberships (UserID, RoomID) VALUES (?,?)", [otherUserID, roomID]);
+    },
+
+    addUserToRoom(userID, roomID) {
+        let res = db.run("INSERT INTO RoomMemberships (UserID, RoomID) VALUES (?,?)", [userID, roomID]);
+    },
+
+    leaveRoom(userID, roomID) {
+        let res = db.run("DELETE FROM RoomMemberships WHERE UserID = ? AND RoomID = ?", [userID, roomID]);
+    },
+
+    /**
+     * Creates and returns the message on success
+     */
+    createMessage(userID, roomID, content) {
+        let res = db.run(
+            "INSERT INTO Messages (Sender, RoomID, Timestamp, Content) VALUES [?,?,?,?]"
+            [userID, roomID, Date.now(), content]
+        );
+        return res.length == 1 ? {
+            Sender: userID,
+            RoomID: roomID,
+            Timestamp: Date.now(), // yes, this will be slightly different.
+            Content: content
+        } : undefined;
+    },
+
+    userExists(username) {
+        let res = db.run("SELECT * FROM Users WHERE Username = ?", ['Holst']);
+        return res.length == 1;
+    }
+
+}
+
+db.connect('database/database.db')
+module.exports = facade;
--- AndroidChatServer/main.js
+++ AndroidChatServer/main.js
@@ -1,25 +1,106 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database.db', (err) => {
-    if(err != null) {
-        console.log("Opening database failed!");
-    }
-}); 
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const db = require('./database');
+const sha1 = require('sha1');
+
 
 io.on('connection', (client) => {
 
-    client.on('joinRoom', (data) => {
-        client.join(data.roomId);
+    client.on('register', (data) => {
+        let username = data.username;
+        let password = data.password;
+        let nickname = data.nickname;
+
+        if (username == undefined || password == undefined) {
+            client.send('registrationError', {
+                message: 'Please supply username and password'
+            })
+        }
+
+        if (nickname == undefined) {
+            nickname = username;
+        }
+
+        if (db.createUser(username, sha1(password), nickname) != -1) {
+            client.send('registrationSuccess')
+        } else {
+            client.send('registrationError', {
+                message: 'Username already taken.'
+            })
+        }
+
     });
 
-    client.on('sendMessage', (data) => {
-        // Data should probably contain roomId and message
+    client.on('authenticate', (data) => {
+        let username = data.username;
+        let password = data.password;
+
+        if (username && password) {
+            const USER = db.auth(username, sha1(password));
+
+            if (!USER) {
+                client.send({
+                    status: 'error',
+                    message: 'Username or password incorrect.'
+                });
+                return;
+            }
+
+            //we're authenticated, add event listeners.
+
+            client.on('createRoom', (data) => {
+                let otherUserID = data.otherUserID;
+                if (!otherUserID) {
+                    return;
+                }
+                db.createRoom(USER.ID, data.otherUserID);
+
+            });
+
+            client.on('addToRoom', (data) => {
+                let userID = data.userID;
+                let roomID = data.roomID;
+                if (userID && roomID) {
+                    db.addUserToRoom(userID, roomID);
+                }
+            });
+
+            client.on('leaveRoom', (data) => {
+                let roomID = data.roomID;
+                if (!roomID) {
+                    return;
+                }
+                db.leaveRoom(USER.ID, roomID);
+                client.send('roomLeft', { roomID: roomID });
+                io.to(roomID).emit('userLeft', USER);
+            });
+
+            client.on('sendMessage', (data) => {
+                let message = data.message;
+                let roomID = data.roomID;
+                if(message && roomID){
+                    let msg = db.createMessage(USER.ID, roomID, message);
+                    io.to(roomID).emit('message', msg)
+                }
+
+            });
+
+            client.on('joinRoom', (data) => {
+                let roomID = data.roomID;
+                if(roomID && db.userIsInRoom(USER.ID, roomID)){
+                    client.join(roomID);
+                }
+
+            });
+
+            client.on('disconnect', function () {
+                // Do something, maybe notify other users that this user is now offline.
+            });
+
+            client.send('authenticationSuccess');
+        }
     });
 
-    client.on('disconnect', function () {
-        // Do something, maybe notify other users that this user is now offline.
-    });
 });
 
 server.listen(3000);
