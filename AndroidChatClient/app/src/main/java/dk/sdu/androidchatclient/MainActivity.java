57853a261452efa0f4550d8fa5a8b894fb7f8fe0
Holst <moviemastersdk@gmail.com>
2018-05-20 11:30:40 +0200

    Login Screen |Socket.io dependency

--- AndroidChatClient/app/src/main/java/dk/sdu/androidchatclient/MainActivity.java
+++ AndroidChatClient/app/src/main/java/dk/sdu/androidchatclient/MainActivity.java
@@ -1,5 +1,6 @@
 package dk.sdu.androidchatclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 
     @Override
